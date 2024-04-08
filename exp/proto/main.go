9312e86b2578b6f550acbeef00af6e3fb7cffc06
Karol Moroz <karol@moroz.dev>
2024-04-08 20:09:02 +0800

    Encode and decode

--- exp/proto/main.go
+++ exp/proto/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/moroz/saboru-server/exp/proto/pb"
+	"google.golang.org/protobuf/proto"
+)
+
+func main() {
+	channel := pb.Channel{
+		Id:    42,
+		Label: "general",
+	}
+
+	out, err := proto.Marshal(&channel)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%#v\n", out)
+
+	decoded := &pb.Channel{}
+	err = proto.Unmarshal(out, decoded)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%#v\n", decoded)
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/moroz/saboru-server
 go 1.22.1
 
 require (
-	github.com/go-chi/chi/v5 v5.0.12 // indirect
-	github.com/jmoiron/sqlx v1.3.5 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/go-chi/chi/v5 v5.0.12
+	google.golang.org/protobuf v1.33.0
 )
